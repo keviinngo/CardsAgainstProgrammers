048f2912ecb832a89445d66ce164c2263b0e1211
thebirk <pingnor@gmail.com>
2020-01-28 11:57:38 +0100

    joinpage

---
+++ lib/screens/join.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// Interface to join a already running game.
+class JoinPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Homepage')
+      ),
+    );
+  }
+}
